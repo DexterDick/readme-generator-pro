c31b6392f0ca5b55bf678f0dadca8acba237cb2a
Dexter Dick <@dexterdick@hotmail.com>
2022-12-30 17:17:07 -0500

    completed questions for readme

--- index.js
+++ index.js
@@ -1,13 +1,132 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please provide title of your repository? (Required)",
+        validate: (nameInput) => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Title field is required please enter.");
+                return false;
+            }
+        },
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide description of your repository? (Required)",
+        validate: (nameInput) => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("description field is required please enter.");
+                return false;
+            }
+        },
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide installation steps for project. (Required)",
+        validate: (installationInput) => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log("Please enter your installation instructions!");
+                return false;
+            }
+        },
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide usage instructions for project. (Required)",
+        validate: (usageInput) => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log("Please enter usage instructions!");
+                return false;
+            }
+        },
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license would you like to use for project?",
+        choices: ["mit", "apache", "No License"],
+    },
+    {
+        type: "confirm",
+        name: "contributeToProject",
+        message: "Would you like to developers to contribute to project?",
+        default: true,
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message:
+            "Please provide information on how to contribute to project. (Required)",
+        when: ({ contributeToProject }) => {
+            if (contributeToProject) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: (contributeInput) => {
+            if (contributeInput) {
+                return true;
+            } else {
+                console.log("Please enter how to contribute to project!");
+                return false;
+            }
+        },
+    },
+
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide testing instructions. (Required)",
+        validate: (testsInput) => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log("Please enter testing instructions!");
+                return false;
+            }
+        },
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Please provide questions. (Required)",
+        validate: (questionsInput) => {
+            if (questionsInput) {
+                return true;
+            } else {
+                console.log("Please enter questions!");
+                return false;
+            }
+        },
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer
+        .prompt(questions)
+        .then((readmeData) => console.log(readmeData));
+}
 
 // Function call to initialize app
 init();
